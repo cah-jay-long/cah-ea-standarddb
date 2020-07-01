fbd2a198212000dbe0c8ee03bcb388f9743adafc
Jay Long <jay.long@cardinalhealth.com>
2020-07-01 10:23:54 -0400

    Inital Prpject Load

---
+++ src/main/java/com/cah/eit/ea/app/standarddb/StandarddbApplication.java
@@ -0,0 +1,13 @@
+package com.cah.eit.ea.app.standarddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandarddbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandarddbApplication.class, args);
+	}
+
+}
